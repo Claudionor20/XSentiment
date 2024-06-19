b2729ee00d55611ab699fd0d6e8684f55561cc70
Claudionor Junior <123711704+Claudionor20@users.noreply.github.com>
2024-06-19 18:50:34 -0300

    modelo com problema

--- texto_d4.R
+++ texto_d4.R
@@ -7,8 +7,10 @@ library(stringi)
 library(wordcloud)
 library(textstem)
 library(rtweet)
-
-
+library(caret)
+library(lexiconPT)
+library(maditr)
+library(xgboost)
 
 # Lendo base
 load("dados_rotulados.rda")
@@ -30,6 +32,13 @@ stopwords <- as.vector(stopwords$V1)
 # Função de remoção de acentos
 remove_acentos <- function(text) {
   text <- stri_trans_general(text, "latin-ascii")
+  
+  return(text)
+}
+
+# função para remover numeros
+remove_numeros <- function(text) {
+  text <- gsub("[0-9]", "", text)
   
   return(text)
 }
@@ -64,10 +73,13 @@ dados <- dados|>
   mutate(word = remove_acentos(word))
 
 
-
 # Removendo stopwords
 dados_filtrado <- dados|>
   filter(!word %in% stopwords)
+
+# Removendo numeros
+dados_filtrado <- dados_filtrado|>
+  mutate(word = remove_numeros(word))
 
 
 
@@ -207,7 +219,7 @@ dados_min<-dplyr::filter(dados_lem,
 
 
 # contando frequencia das palavras dentro do tweet
-dados_freq = group_by(dados_min,RecordID,word)
+dados_freq = group_by(dados_min,RecordID,word,Polaridade)
 freq_palavra = summarize(dados_freq,word,frequencia=n())
 freq_palavra = unique(freq_palavra)
 dados_freq = ungroup(dados_freq)
@@ -219,12 +231,158 @@ freq_palavra = mutate(freq_palavra,FreqPercentual = frequencia/TotalPalavras)
 
 dados_freq = select(freq_palavra,-c(frequencia,TotalPalavras))
 
+# Criando matriz termo documento com polaridade
+tdm = dcast(dados_freq, RecordID + Polaridade ~ word, value.var = "FreqPercentual")
 
-tdm <- dados_freq %>%
-  tidyr::spread(key = word, value = FreqPercentual, fill = 0)
+tdm<-replace(tdm,is.na(tdm), 0)
+
+##Realizar analise de sentimento
+#carregando dicionario lexico
+oplexicon = lexiconPT::oplexicon_v3.0
+names(oplexicon) = c('word','Classe','Polaridade Palavra','Polaridade Revisada')
+
+#Vamos usar o dados_lem, pois possui o tweet
+#prox do seu formato inicial (sem cortes por freq)
+dados_sent = dplyr::left_join(dados_lem,oplexicon,by='word')
+dados_sent$`Polaridade Palavra` = replace(dados_sent$`Polaridade Palavra`,is.na(dados_sent$`Polaridade Palavra`), 0)
+dados_sent = dplyr::group_by(dados_sent, RecordID)
+scores = dplyr::summarize(dados_sent,score=sum(`Polaridade Palavra`))
 
 
+#termo-documento por frequncia (Bag of Words)
+matriz_ftd = dplyr::left_join(tdm,scores,by='RecordID')
+matriz_ftd = select(matriz_ftd,c(Polaridade,everything()))
+# Tirando recordeID
+matriz_ftd = select(matriz_ftd,-RecordID)
+
+# Transformar coluna polaridade em 0 e 1
+matriz_ftd$Polaridade = ifelse(matriz_ftd$Polaridade == "-1", 0, 1)
+# Separando em treino e teste
+
+set.seed(123)
+matriz_ftd$Polaridade = as.factor(matriz_ftd$Polaridade)
+notreino <- createDataPartition(matriz_ftd$Polaridade, p = 0.7, list = FALSE)
+treino <- matriz_ftd[notreino,]
+teste <- matriz_ftd[-notreino,]
+
+sapply(treino, class)
 
 
+otimizador <- function(predicao, resposta) {
+  require(ROCR)
+  
+  # Criar o objeto de previsão
+  pred <- prediction(predicao, resposta)
+  
+  # Calcular a performance
+  perf <- performance(pred, "tpr", "fpr")
+  
+  # Calcular a diferença entre TPR e FPR
+  tpr <- unlist(perf@y.values)
+  fpr <- unlist(perf@x.values)
+  funcao <- tpr - fpr
+  
+  # Encontrar o ponto de corte ótimo
+  optimal_idx <- which.max(funcao)
+  ponto_otimo <- perf@alpha.values[[1]][optimal_idx]
+  
+  # Plotar a curva ROC
+  plot(perf, colorize = TRUE, lwd = 2, main = "Curva ROC")
+  abline(a = 0, b = 1, lty = 2, col = "gray")
+  points(fpr[optimal_idx], tpr[optimal_idx], pch = 19, col = "red")
+  text(fpr[optimal_idx], tpr[optimal_idx], labels = round(ponto_otimo, 2), pos = 4, col = "red")
+  
+  return(ponto_otimo)
+}
+
+
+# Definir a grade de hiperparâmetros
+param_grid <- expand.grid(
+  eta = c(0.3),
+  max_depth = c(6),
+  nrounds = c(1000),
+  early_stopping_rounds = c(10)
+)
+
+results <- list()
+
+
+# Definir o número de folds para a validação cruzada
+k <- 10
+folds <- caret::createFolds(treino$Polaridade, k = k, list = TRUE)
+
+# Função para calcular a média das métricas de cada combinação de hiperparâmetros
+calc_media_metrics <- function(metrics_list) {
+  media_metrics <- sapply(metrics_list, mean)
+  return(media_metrics)
+}
+
+
+# Loop sobre todas as combinações de hiperparâmetros
+for (i in 1:nrow(param_grid)) {
+  params <- param_grid[i, ]
+  
+  # Inicializar listas para armazenar métricas de cada fold
+  metrics_list <- list(accuracy = numeric(k),
+                       specificity = numeric(k),
+                       sensitivity = numeric(k),
+                       npv = numeric(k))
+  
+  for (fold_index in 1:k) {
+    fold <- folds[[fold_index]]
+    
+    # Separar dados de treino e validação
+    treino_fold <- treino[-fold,]
+    dtrain <- xgb.DMatrix(data = as.matrix(treino_fold[,-length(treino_fold)]), label = as.matrix(as.factor(treino_fold$Polaridade)))
+    
+    validacao_fold <- treino[fold,]
+    dvalid <- xgb.DMatrix(data = as.matrix(validacao_fold[,-length(validacao_fold)]), label = as.matrix(as.factor(validacao_fold$Polaridade)))
+    
+    # Treinar o modelo com o fold de treino
+    set.seed(5415)
+    xgbm_model <- xgboost(data = dtrain,
+                          gamma=0, eta=params$eta, max_depth=params$max_depth,
+                          nrounds = params$nrounds, 
+                          early_stopping_rounds=params$early_stopping_rounds,
+                          objective = "binary:hinge",
+                          verbose = 0)
+    
+    # Predição na validação
+    predicao_validacao <- predict(xgbm_model, newdata = dvalid)
+    
+    # Encontrar o ponto de corte ótimo
+    ponto_otimo <- otimizador(predicao_validacao, validacao_fold$Polaridade)
+    
+    # Classificar as predições
+    classificacao <- ifelse(predicao_validacao >= ponto_otimo, 1, 0)
+    
+    # Matriz de confusão
+    #cm <- confusionMatrix(as.factor(classificacao), as.factor(validacao_fold$Polaridade))
+    cm <- confusionMatrix(as.factor(classificacao),as.factor(validacao_fold$Polaridade))
+    
+    # Armazenar métricas do fold
+    metrics_list$accuracy[fold_index] <- cm$overall['Accuracy']
+    metrics_list$specificity[fold_index] <- cm$byClass['Specificity']
+    metrics_list$sensitivity[fold_index] <- cm$byClass['Sensitivity']
+    metrics_list$npv[fold_index] <- cm$byClass['Neg Pred Value']
+  }
+  
+  # Calcular médias das métricas
+  media_metrics <- calc_media_metrics(metrics_list)
+  
+  # Armazenar os resultados
+  results[[i]] <- list(
+    params = params,
+    accuracy = media_metrics['accuracy'],
+    specificity = media_metrics['specificity'],
+    sensitivity = media_metrics['sensitivity'],
+    negative_predictive_value = media_metrics['npv'],
+    ponto_otimo = ponto_otimo,
+    model = xgbm_model,
+    confusion_matrix = cm
+  )
+  
+  
+}
 
 
